2a0a66bdc4495f131e637d8faa072c2e8ca23882
TothMarcel05 <toth.marcel.daniel@diak.szbi-pg.hu>
2024-11-29 11:18:27 +0100

    :D

--- app.py
+++ app.py
@@ -2,18 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
 
-@app.route('/tabla')
-def tabla():
-    plants = [
-        {"name": "Monstera", "type": "Szobanövény", "feature": "Nagyméretű levelek"},
-        {"name": "Bambusz", "type": "Dísznövény", "feature": "Gyors növekedés"},
-        {"name": "Kaktusz", "type": "Pozsgás", "feature": "Szárazságtűrő"}
-    ]
-    return render_template('tabla.html', plants=plants)
+
+@app.route('/index', methods=["GET"])
+def index():
+    return render_template('add.html', adds=adds)
+
+@app.route('/add', methods=["GET", "POST"])
+def add_order():
+    if request.method == 'POST':
+        _name = request.form.get('name')
+        _prize = request.form.get('prize')
+        _category = request.form.get('category')
+        add = {
+            'name': _name,
+            'prize': _prize,
+            'category': _category}
+        adds.append(add)
+    return render_template('index.html')
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
